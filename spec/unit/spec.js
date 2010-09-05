976bd03abd2050c0a0da830e5f0dacded67bcce3
Gustaf Nilsson Kotte <gustaf.nk@jayway.com>
2010-09-05 11:58:34 +0200

    Stubbed out getByNr

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,7 +1,21 @@
 
 describe 'YourLib'
+  
   describe '.someMethod()'
     it 'should do something'
+      $("#shell").html($(fixture('Movielist.html')))
+      result = fixture('Butikslager_available.html')
+
+      
+      MovieInfoRepository = function(){
+        this.getByNr = function() {
+          return result
+        }
+      }
+
+      var x = new TooBigClass();
+      x.hideAllIfNotInStore();
+
       true.should.be true
     end
   end
